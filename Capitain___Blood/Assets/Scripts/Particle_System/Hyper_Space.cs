555f83b78253cd5550dc535ded73629293a34c99
Kilian <k.allaire@rubika-edu.com>
2019-03-29 17:26:03 +0100

    FTL_VFX

--- Capitain___Blood/Assets/Scripts/Particle_System/Hyper_Space.cs
+++ Capitain___Blood/Assets/Scripts/Particle_System/Hyper_Space.cs
@@ -11,15 +11,17 @@ namespace RetroJam.CaptainBlood
 
         [SerializeField] ParticleSystem particleSystemStay;
         [SerializeField] ParticleSystem particleSystemLoop;
+        [SerializeField] ParticleSystem particleSystemEnd;
 
         [SerializeField] bool activated = false;
+        [SerializeField] float timeWarp;
 
 
 
         // Start is called before the first frame update
         void Start()
         {
-
+            particleSystemEnd.Pause();
         }
 
         // Update is called once per frame
@@ -56,23 +58,42 @@ namespace RetroJam.CaptainBlood
 
 
 
+
         }
 
         void Activation()
         {
 
-            particleSystemStay.Play();
-            particleSystemLoop.Play();
+            StartCoroutine(EndParticleEffect());
 
+        }
+
+        IEnumerator EndParticleEffect()
+        {
+
+            particleSystemStay.Play();
+            particleSystemLoop.Emit(1);
 
             var main = particleSystemStay.main;
-            main.startLifetime = 2;
+            main.startDelay = 10;
 
-            var main2 = particleSystemLoop.main;
-            main2.startLifetime = 2;
+            yield return new WaitForSeconds(timeWarp);
+
+            var mainloop = particleSystemLoop.main;
+            mainloop.startLifetime = 0;
+
+            yield return new WaitForSeconds(1);
+
+            particleSystemEnd.Play();
+
+            var mainEnd = particleSystemEnd.main;
+            mainEnd.startLifetime = 2;
 
 
+            yield return new WaitForSeconds(1);
 
+            mainEnd.startDelay = 10;
+            activated = false;
 
         }
     }
