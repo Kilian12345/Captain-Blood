9574d938324097303adcec6215ef9f420b32ab0f
Kilian <k.allaire@rubika-edu.com>
2019-04-09 23:58:05 +0200

    Cursor

--- Capitain___Blood/Assets/Scripts/Kilian/landing_Control.cs
+++ Capitain___Blood/Assets/Scripts/Kilian/landing_Control.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RetroJam.CaptainBlood
 {
     public class landing_Control : MonoBehaviour
     {
         [SerializeField] TerrainGenerator[] terGen;
-        [SerializeField] GameObject UiImage;
+        [SerializeField] RectTransform UiImage;
+
+        [Space]
+        [Header ("Value")]
 
         [SerializeField]float speed;
         [SerializeField]float moveVert;
         [SerializeField]float moveHori;
         [SerializeField]float moveFor;
 
+        float moveHoriCursor;
         float y;
+        float imageX;
         float pointA;
         float pointB;
         float currentObjective;
@@ -54,6 +60,7 @@ namespace RetroJam.CaptainBlood
         {
             LandingControl();
             CameraBehavior();
+            Curseur();
 
             for (int i = 0; i < terGen.Length; i++)
             {
@@ -67,6 +74,7 @@ namespace RetroJam.CaptainBlood
             float oldMoveVert = moveVert;
             moveVert = Input.GetAxis("Vertical");
             moveHori += Input.GetAxis("Horizontal") * speed;
+            moveHoriCursor = Input.GetAxis("Horizontal") * 2;
             moveFor += (1-Mathf.Abs(Input.GetAxis("Forward"))) * speed * variableSpeed;
 
             ////////////////////////// Camera mouv
@@ -95,15 +103,16 @@ namespace RetroJam.CaptainBlood
             transRotate.z = 0;
             transRotate.w = 0;
             transform.localRotation = Quaternion.Euler(transRotate.x, 0f, 0f); 
-
-            Debug.Log(transform.localRotation);
         }
         void Curseur()
         {
-            float imageX;
-            imageX = UiImage.transform.localPosition.x + moveHori;
-            imageX = Mathf.Clamp(y, 0, 500);
-            UiImage.transform.localPosition = new Vector3(imageX, UiImage.transform.localPosition.y, UiImage.transform.localPosition.z);
+
+            imageX = UiImage.localPosition.x + moveHoriCursor;
+            imageX = Mathf.Clamp(imageX, -100, 100);
+            UiImage.localPosition = new Vector3(imageX, UiImage.localPosition.y, UiImage.localPosition.z);
+
+            StartCoroutine(GetAxisup());
+    
         }
         void OnCollisionEnter (Collision col)
         {
@@ -111,6 +120,24 @@ namespace RetroJam.CaptainBlood
             {
                 variableSpeed = speed0;
             }
+        }
+
+        IEnumerator GetAxisup()
+        {
+           if (Input.GetAxis("Horizontal") == 0)
+            {
+                Debug.Log("SucePute");
+
+                while(UiImage.localPosition.x == 0)
+                {
+                    if (UiImage.localPosition.x > 0)
+                    {UiImage.localPosition = new Vector3(UiImage.localPosition.x - 0.001f, UiImage.localPosition.y, UiImage.localPosition.z);}
+                    if (UiImage.localPosition.x < 0)
+                    {UiImage.localPosition = new Vector3(UiImage.localPosition.x + 0.001f, UiImage.localPosition.y, UiImage.localPosition.z);}
+                }
+
+                yield return UiImage.localPosition;
+            } 
         }
 
         
