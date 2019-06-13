8fd73bf432d47c63d9ccedd425f8bb03e728748f
elie <e.sarfati@rubika-edu.com>
2019-06-13 22:21:18 +0200

    upcom v2 11%

--- Capitain___Blood/Assets/Scripts/UpcomV2/Sentence.cs
+++ Capitain___Blood/Assets/Scripts/UpcomV2/Sentence.cs
@@ -7,6 +7,7 @@ namespace RetroJam.CaptainBlood.Upcom
     public class Sentence
     {
         public Word[] words;
+        public SentenceElement[] elements;
 
         public Sentence(params Word[] _words)
         {
@@ -16,14 +17,29 @@ namespace RetroJam.CaptainBlood.Upcom
                 words[i] = i < _words.Length ? _words[i] : Word.none;
             }
         }
+
+        private void SetElements()
+        {
+            List<Word> list = words.ToList();
+            int hierarchy = 0;
+
+            for (int i = 7; i >= 0; i--)
+            {
+                if (list[i] == Word.none) list.RemoveAt(i);
+            }
+
+            while (list.Count > 0)
+            {
+                
+            }
+        }
     }
 
-    public class SentenceElement
+    public struct SentenceElement
     {
         public Word word;
         public SentenceElement[] elements;
         public int index;
         public int hierarchy;
-
     }
 }
